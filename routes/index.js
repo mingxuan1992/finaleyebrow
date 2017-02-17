e916d9441c7a69fae9c2302d60bdfa53b6bb252d
mingxuan1992 <han@handeMacBook-Pro.local>
2017-02-17 04:36:50 -0800

    version 0.61 ejs error

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,5 @@
  */
 var ejs = require("ejs");
 exports.index = function(req, res){
-	ejs.renderFile('./views/index.ejs', function(err, result) {
-		// render on success
-		if (!err) {
-			res.end(result);
-		}
-		// render or error
-		else {
-			res.end('An error occurred');
-			console.log(err);
-		}
-	});
-};
+	res.render('index', {});
+};	
